1d87043af75044ea9c86b9f5ae1c9b52ec345366
Thijs Raymakers <thijs@raymakers.nl>
2020-02-09 17:48:21 +0100

    Implement branch free multiplicative inverse

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
 use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
-
-#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Hash)]
 // The reason to use a fixed u8 type instead of generics is to guarantee at compile time that all
 // elements fit in the finite field GF(2^8).
 pub struct GF(pub u8);
 
 impl GF {
     pub fn multiplicative_inverse(self) -> Self {
-        unimplemented!()
+        let mut p = 0;
+
+        for x in 0u8..=255u8 {
+            // If zero, the multiplication is results in GF(1)
+            // If non-zero, the multiplication ends with something different.
+            let y = (self * GF(x)).0 ^ 1;
+
+            // OR all bits together in the rightmost bit. If y is zero, that means that the
+            // result of ORing all bits together will also be zero. Otherwise, it will be 1.
+            let or = y | y >> 1 | y >> 2 | y >> 3 | y >> 4 | y >> 5 | y >> 6 | y >> 7;
+
+            // Extend the bits to the full byte and negate it. This means that the AND will
+            // be zero if the multiplication in y was 1.
+            p ^= !extend_bit(or) & x;
+        }
+
+        GF(p)
     }
+}
+
+fn extend_bit(input: u8) -> u8 {
+    (((input) as i8) << 7).wrapping_shr(7) as u8
 }
 
 impl Add for GF {
@@ -56,7 +75,7 @@ impl Mul for GF {
         for _ in 0..8 {
             // If the rightmost bit of b is set, exclusive OR the product p by the value of a.
             // This is polynomial addition.
-            p ^= ((((b & 1) as i8) << 7).wrapping_shr(7) as u8) & a;
+            p ^= extend_bit(b & 1) & a;
 
             // Shift b one bit to the right, discarding the rightmost bit, and making the leftmost
             // bit have a value of zero. This divides the polynomial by x, discarding the x0 term.
@@ -74,7 +93,7 @@ impl Mul for GF {
             // number 0x1b (00011011 in binary). 0x1b corresponds to the irreducible polynomial with
             // the high term eliminated. Conceptually, the high term of the irreducible polynomial
             // and carry add modulo 2 to 0.
-            a ^= ((((carry & 1) as i8) << 7).wrapping_shr(7) as u8) & 0x1b;
+            a ^= extend_bit(carry & 1) & 0x1b;
         }
 
         // p now has the product
@@ -93,6 +112,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_multiplicative_inverse() {
+        assert_eq!(GF(0x01).multiplicative_inverse(), GF(0x01));
+        assert_eq!(GF(0x02).multiplicative_inverse(), GF(0x8d));
+        assert_eq!(GF(0x03).multiplicative_inverse(), GF(0xf6));
+        assert_eq!(GF(0x04).multiplicative_inverse(), GF(0xcb));
+        assert_eq!(GF(0x05).multiplicative_inverse(), GF(0x52));
+        assert_eq!(GF(0x06).multiplicative_inverse(), GF(0x7b));
+        assert_eq!(GF(0xff).multiplicative_inverse(), GF(0x1c));
+    }
+
+    #[test]
     fn test_shift_behaviour() {
         let mut x: i8 = 1;
         x <<= 7;
@@ -105,6 +135,17 @@ mod tests {
         assert_eq!(x as u8, 0b0000_0000 as u8);
         x = x.wrapping_shr(7);
         assert_eq!(x as u8, 0b0000_0000 as u8);
+    }
+
+    #[test]
+    fn test_extend_bit() {
+        assert_eq!(extend_bit(1), 0xff);
+        assert_eq!(extend_bit(0), 0x00);
+        assert_eq!(extend_bit(0b0000_0001), 0xff);
+        assert_eq!(extend_bit(0b0000_0000), 0x00);
+        assert_eq!(extend_bit(0b1000_0100), 0x00);
+        assert_eq!(extend_bit(0b0100_0100), 0x00);
+        assert_eq!(extend_bit(0b1100_0101), 0xff);
     }
 
     #[test]
